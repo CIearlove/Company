a002656a30cd9bd5257bee43d2499ad12b84dc49
junranChen <617316241@QQ.com>
2018-07-29 15:44:04 +0800

    Delete dbo.Staff.sql
    上传错了。

--- dbo.Staff.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Staff] (
-    [Id]   INT           IDENTITY(1,1) NOT NULL ,
-    [Name] NVARCHAR (100) NOT NULL,
-    [Age]  INT           NOT NULL,
-    [Sex]  CHAR (10)      NOT NULL,
-    CONSTRAINT [PK_Staff] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
